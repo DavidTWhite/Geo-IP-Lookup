2c92945f4dec3e101b8ed748c29984fee4fd2b23
David White <david.white323@gmail.com>
2017-04-22 12:26:01 -0600

    First version that sort of works

--- ip_tool/file_parser.py
+++ ip_tool/file_parser.py
@@ -1,20 +1,39 @@
 import sys
+import re
+
+regex = re.compile("""(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])""")
+
+def parseChunk(inputStr):
+    addressList = []
+    matches = regex.findall(inputStr)
+    for address in matches:
+        addressList.append(address)
+    return addressList
 
 if __name__ == '__main__':
     filename = None
+    inFile = None
+    ipaddresses = []
     try:
         filename = sys.argv[1]
         inFile = open(filename, 'r')
     except Exception as e:
         print e
+    else:
+        chunk = inFile.read(1024)
+        while chunk:
+            addresses = []
+            addresses = parseChunk(chunk)
+            if addresses:
+                 ipaddresses.append(addresses)
+            #backtrack 16 bytes so we can't miss an IP
+            #by splitting it during the read
+            if len(chunk) == 1024:
+                inFile.seek(-16, 1)
+            chunk = inFile.read(1024)
 
-    #Read some bytes, process some bytes.
-    #Handle an IP address in the middle of the read chunk...
-    #Maybe look for something that looks like the start of an IP address
-    #and then read some bytes after that and regex the thing out of it.
-
-    #How do we handle IP addresses that are right next to each other?
+    print ipaddresses
     try:
         infile.close()
-    except:
-        pass
+    except Exception as e:
+        print e
