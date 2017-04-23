31e86c25ee027cadfd86014b68eda30ef318e07d
David White <david.white323@gmail.com>
2017-04-23 13:09:42 -0600

    Add rdap lookup functionality with rdap.arin.net

--- ip_tool/rdap_lookup.py
+++ ip_tool/rdap_lookup.py
@@ -1,3 +1,18 @@
+import requests
+import json
+
 class RDAPLookup(object):
     def __init__(self):
-        pass
+        self.rdapKeys = [u'handle', u'name', u'links', u'entities', u'port43',
+                         u'endAddress', u'parentHandle', u'ipVersion', u'startAddress',
+                          u'rdapConformance', u'notices', u'objectClassName', u'events']
+
+    def makeRDAPRequest(self,ip):
+        return requests.get("http://rdap.arin.net/bootstrap/ip/" + ip)
+
+    def getRDAPDict(self, ip):
+        jsonData = self.makeRDAPRequest(ip)
+        return json.loads(jsonData.text.encode('ascii','ignore'))
+
+    def getBulkRDAPInfo(self, ipList):
+        return [self.getRDAPDict(ip) for ip in ipList]
