e8e4b1f94a3d46557f3d59290624500d79220132
David White <david.white323@gmail.com>
2017-04-23 13:08:01 -0600

    use multiprocessing Pool to make requests for each IP for performance reasons

--- ip_tool/iptool.py
+++ ip_tool/iptool.py
@@ -4,6 +4,19 @@ from ip_filter import IPFilter
 from rdap_lookup import RDAPLookup
 
 import sys
+from multiprocessing import Pool
+
+def mergeDicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
+
+def getCombinedDataDict(ip):
+    print "ip", ip
+    geolookup = GeoIP()
+    rdaplookup = RDAPLookup()
+    combinedDict = mergeDicts(geolookup.getLocationDict(ip), rdaplookup.getRDAPDict(ip))
+    return combinedDict
 
 if __name__ == '__main__':
     try:
@@ -14,8 +27,15 @@ if __name__ == '__main__':
     parser = FileParser()
     ipAddresses = parser.parseFile(filename)
     print len(ipAddresses), "IP addresses found in file. Performing GeoIP and RDAP Lookup"
-    geolookup = GeoIP()
-    ipLookupData = geolookup.getBulkLocationInfo(ipAddresses)
-    print ipLookupData
-    #rdap = RDAPLookup()
-    #rdapData = rdap.performQueries(ipAddresses)
+    combinedDataList = []
+    p = Pool(8)
+    results = p.map(getCombinedDataDict, ipAddresses)
+    p.close()
+    p.join()
+
+    print results
+    # for ip in ipAddresses:
+    #     print "pulling data for ", ip
+    #     threads.append(threading.Thread(target = getCombinedDataDict, args=(ip, globalResultList)))
+    #
+    # print globalResultList
