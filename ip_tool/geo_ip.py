ed7a12528493158aa195cf71f17dc1f208572710
David White <David White>
2017-04-30 23:01:46 -0600

    Refactoring geo ip information provider. Add abstract base class and derived classes for maxmind and freegeoip.net providers

--- ip_tool/geo_ip.py
+++ ip_tool/geo_ip.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import abc
+import geopip2.database as geodb
 
 class GeoIP(object):
     """Make requests for geo IP lookups from freegeoip.net"""
@@ -26,5 +28,56 @@ class GeoIP(object):
             returnDict['GeoIPError'] = str(e)
         return returnDict
 
-class LocalGeoIPProvider(object):
-    pass
+class FreeGeoIPProvider(BaseGEOIPProvider):
+    """ Make requests to the freegeoip.net API for IP information """
+    def getLocationDict(self, ip):
+        jsonInfo = requests.get("http://freegeoip.net/json/" + ip)
+        return json.loads(jsonInfo.text.encode('ascii','ignore'))
+
+    def getCity(self, ip):
+        return self.getLocationDict(ip)['city']
+
+    def getCountry(self, ip):
+        return self.cityDB.city(ip).city.country.name
+
+    def getLatLon(self, ip):
+        return (self.cityDB.city(ip).location.latitude,
+                self.cityDB.city(ip).location.longitude,
+                self.cityDB.city(ip).location.accuracy_radius)
+
+class MaxMindIPProvider(BaseGEOIPProvider):
+    """ Access a maxmind geoip2 City database for IP information """
+    def __init__(self, dbFilename):
+        self.cityDB = geodb.Reader(dbFilename)
+
+    def getMaxMindCity(self, ip):
+        return self.cityDB.city(ip)
+
+    def getCity(self, ip):
+        return self.cityDB.city(ip).city.name
+
+    def getCountry(self, ip):
+        return self.cityDB.city(ip).city.country.name
+
+    def getLatLon(self, ip):
+        return (self.cityDB.city(ip).location.latitude,
+                self.cityDB.city(ip).location.longitude,
+                self.cityDB.city(ip).location.accuracy_radius)
+
+class BaseGEOIPProvider(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def getCity(self, ip): return
+
+    @abc.abstractmethod
+    def getCountry(self, ip): return
+
+    @abc.abstractmethod
+    def getLatLon(self, ip): return
+
+    @abc.abstractmethod
+    def getContinent(self, ip): return
+
+    @abc.abstractmethod
+    def getLocationSubdivisions(self, ip): return
