d12d82afc2de18d725007e02e4650015a08a8a8b
DavidTWhite <david.white323@gmail.com>
2017-06-11 15:10:50 -0600

    Remove old geoip class and old text based interface.
    Preparation for writing GUI version of tool

--- ip_tool/iptool.py
+++ ip_tool/iptool.py
@@ -1,4 +1,4 @@
-from geo_ip import GeoIP, MaxMindIPProvider
+from geo_ip import MaxMindIPProvider
 from file_parser import FileParser
 from ip_filter import IPFilter
 from rdap_lookup import RDAPLookup
@@ -7,16 +7,12 @@ import sys
 from multiprocessing import Pool
 import json
 
+import wx
+
 def mergeDicts(x, y): #Built in way to combine two dictionaries is a python 3 feature
     z = x.copy()
     z.update(y)
     return z
-
-def getCombinedDataDict(ip):
-    geolookup = GeoIP()
-    rdaplookup = RDAPLookup()
-    combinedDict = mergeDicts(geolookup.getLocationDict(ip), rdaplookup.getRDAPDict(ip))
-    return combinedDict
 
 def getRDAP(ip):
     rdaplookup = RDAPLookup()
@@ -38,44 +34,7 @@ def rdapLookup(ipList):
     return results
 
 if __name__ == '__main__':
-    try:
-        filename = sys.argv[1]
-        print "Parsing", filename
-    except IndexError:
-        filename = raw_input("Please enter a filename to parse IP Addresses from: ")
-    parser = FileParser()
-    ipAddresses = parser.parseFile(filename)
-    print len(ipAddresses), "IP addresses found in file. Performing GeoIP Lookup"
-
-    mmIP = MaxMindIPProvider('..\\geoipdb\\GeoLite2-City_20170502\\GeoLite2-City.mmdb')
-    geoList = [{'ip':ip} for ip in ipAddresses]
-    for each in geoList:
-        each['city'] = mmIP.getCity(each['ip'])
-    print "GeoIPResults: ", geoList
-
-    print "Doing RDAP lookup"
-    results = rdapLookup(ipAddresses)
-    print "Finished RDAP lookup"
-
-    queryTool = IPFilter(results)
-    queryTool.printHelp()
-    while True:
-        print
-        print "Type 'exit' to quit and 'help' to see query help"
-        print
-        userInput = raw_input("Enter Your Query: ")
-        if userInput.lower() == 'exit':
-            break
-        elif userInput.lower() == 'help':
-            queryTool.printHelp()
-        else:
-            try:
-                userData = queryTool.userQuery(userInput)
-            except Exception as e:
-                print "Error in query:", e
-            else:
-                for record in userData:
-                    print "-------------------"
-                    for requestedKey in record:
-                        print requestedKey[0],':', requestedKey[1]
-                    print "-------------------"
+    app = wx.App()
+    frame = wx.Frame(None, -1, 'iptool.py')
+    frame.Show()
+    app.MainLoop()
