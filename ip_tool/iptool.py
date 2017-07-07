15f033eabebfddf465ad0ce8b371fe1febde55df
David White <david.white323@gmail.com>
2017-07-06 19:08:37 -0600

    Remove unused legacy helper methods

--- ip_tool/iptool.py
+++ ip_tool/iptool.py
@@ -11,29 +11,9 @@ import wx
 import pyslip
 import pyslip.gmt_local_tiles as tiles
 
-def mergeDicts(x, y): #Built in way to combine two dictionaries is a python 3 feature
-    z = x.copy()
-    z.update(y)
-    return z
-
 def getRDAP(ip):
     rdaplookup = RDAPLookup()
     return rdaplookup.getRDAPDict(ip)
-
-def rdapLookup(ipList):
-    rdaplookup = RDAPLookup()
-    results = []
-    p = Pool(8)     #Using fewer workers reduces risk of connection refusal from lookup servers
-    numTasks = len(ipList)
-    for i, result in enumerate(p.imap_unordered(getRDAP, ipList), 1):
-        try:
-            sys.stderr.write('\rProgress: {0:.2%}'.format(i/float(numTasks)))
-            results.append(result)
-        except Exception as e:
-            print e
-    p.close()
-    p.join()
-    return results
 
 mmIP = MaxMindIPProvider('..\\geoipdb\\GeoLite2-City_20170502\\GeoLite2-City.mmdb')
 def getIP(ip):
